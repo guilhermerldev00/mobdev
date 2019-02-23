be1dd888a31c860e2e278accdd5218a893c9886b
Guilherme Rodrigues <guigar00d@gmail.com>
2019-02-22 23:11:41 -0300

    inicio do projeto

---
+++ project/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+    
+    <section id="top">
+      <header>
+        <div class="container-fluid">
+          <div id="header" class="row">
+            <div id="logo" class="col-lg-6">
+              <h1>DOT</h1>
+            </div>
+            <ul>
+              <li><a href="#">Sobre nós</a></li>
+              <li><a href="#">Produtos</a></li>
+              <li><a href="#">Soluções</a></li>
+              <li><a href="#">Contatos</a></li>
+            </ul>
+          </div>
+        </div>
+      </header>
+
+      <img id="img-main" src="assets/img/main.png" alt="">
+
+      <section id="sub-title">
+        <div class="container-fluid">
+            <h4>How to accomplish your biggest career goals</h4>
+        </div>
+      </section>
+    </section>
+
+    <section id="content" class="container-custom">
+        <h5>O que você vai aprender</h5>
+        <h6>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipt lobortis nisl ut aliquio commodo consequat.</h6>
+        <ul id="learn">
+          <li id="world"><img src="assets/img/worldwide.png" alt=""><span>Global Thinking</span></li>
+          <li><img src="assets/img/diagram.png" alt=""><span>Mind Setting</span></li>
+          <li><img src="assets/img/flag.png" alt=""><span>Your Future</span></li>
+        </ul>
+    </section>
+  
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+    <link href="assets/css/style.css" rel="stylesheet">
+    <script type="text/javascript" src="assets/js/main.js"></script>
+  </body>
+</html>
